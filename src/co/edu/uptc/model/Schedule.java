1fd73a6437df6728ee33111dd315484ae9ad2e1b
Daniel Campiño <daniel.campino@uptc.edu.co>
2025-04-02 11:35:19 -0500

    Estructura de clases System, Parking, Ticket, Schedule, Vehicle

--- src/co/edu/uptc/model/Schedule.java
+++ src/co/edu/uptc/model/Schedule.java
@@ -1,3 +1,17 @@
 public class Schedule {
 
+    private String day;
+    private String openTime;
+    private String closeTime;
+
+    public Schedule(){
+
+    }
+
+    public Schedule(String day, String openTime, String closeTime){
+        this.day = day;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+    }
+
 }

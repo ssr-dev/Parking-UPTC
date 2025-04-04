34169bffd25bf51e60d2d4af32bbe82a2e214b6f
Ivxnne <ivonne.medina@uptc.edu.co>
2025-04-04 11:31:08 -0500

    get y set vehicle

--- src/co/edu/uptc/model/Vehicle.java
+++ src/co/edu/uptc/model/Vehicle.java
@@ -7,6 +7,22 @@ public class Vehicle {
 
     }
 
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public Ticket getAssignedTicket() {
+        return assignedTicket;
+    }
+
+    public void setAssignedTicket(Ticket assignedTicket) {
+        this.assignedTicket = assignedTicket;
+    }
+
     public Vehicle(String plate, Ticket assignedTicket){
         this.plate = plate;
         this.assignedTicket = assignedTicket;
